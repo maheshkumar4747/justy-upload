a2bb23886c137d1103802ae4614f5cae9c27fd87
maheshkumar4747 <rajbhar81@gmail.com>
2023-05-21 22:06:24 +0530

    normal

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -6,7 +6,7 @@ const Layout = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="react-upload/">Home</Link>
           </li>
           <li>
             <Link to="/blogs">Blogs</Link>
