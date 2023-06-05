4b461901de843b7222719f2942b637e1292bb80e
maheshkumar4747 <rajbhar81@gmail.com>
2023-06-05 08:35:03 +0530

    make title

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -6,7 +6,7 @@ const Layout = () => {
       <nav>
         <ul>
           <li>
-            <Link to="react-upload/">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/blogs">Blogs</Link>
